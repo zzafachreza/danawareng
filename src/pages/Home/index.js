4f1b4b96812b2051d501e36dfe8a6e428762cd78
Fachreza Maulana <zzafachreza@gmail.com>
2023-10-04 16:39:22 +0700

    bismillah

---
+++ src/pages/Home/index.js
@@ -0,0 +1,261 @@
+import { Alert, StyleSheet, Text, View, Image, FlatList, ActivityIndicator, Dimensions, ImageBackground, TouchableWithoutFeedback, TouchableNativeFeedback, Linking } from 'react-native'
+import React, { useState, useEffect, useRef } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { apiURL, getData, MYAPP, storeData } from '../../utils/localStorage';
+import { colors, fonts, windowHeight, windowWidth } from '../../utils';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import { Icon } from 'react-native-elements/dist/icons/Icon';
+import { useIsFocused } from '@react-navigation/native';
+import axios from 'axios';
+import 'intl';
+import 'intl/locale-data/jsonp/en';
+import moment from 'moment';
+import 'moment/locale/id';
+import MyCarouser from '../../components/MyCarouser';
+import { Rating } from 'react-native-ratings';
+
+export default function Home({ navigation, route }) {
+
+
+
+  const [user, setUser] = useState({});
+  const isFocus = useIsFocused();
+  const [data, setData] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [comp, setComp] = useState({});
+
+  const _getTransaction = async () => {
+
+    await getData('user').then(u => {
+      setUser(u);
+    })
+
+    await axios.post(apiURL + 'company').then(res => {
+
+      setComp(res.data.data);
+
+    });
+
+    await axios.post(apiURL + 'menu').then(res => {
+
+      console.log(res.data);
+      setData(res.data);
+
+    });
+  }
+
+
+  useEffect(() => {
+    if (isFocus) {
+      _getTransaction();
+    }
+  }, [isFocus]);
+
+  const __renderItem = ({ item }) => {
+    return (
+      <TouchableWithoutFeedback onPress={() => navigation.navigate(item.modul, item)}>
+        <View style={{
+          flex: 1,
+          padding: 10,
+          borderWidth: 1,
+          borderRadius: 10,
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderColor: colors.secondary,
+          // backgroundColor: colors.white,
+          margin: 5,
+          height: windowHeight / 8,
+        }}>
+
+          <Image source={{
+            uri: item.image
+          }} style={{
+            // flex: 1,
+            width: 40,
+            height: 40,
+            resizeMode: 'contain'
+          }} />
+          <Text style={{
+            marginTop: 10,
+            fontFamily: fonts.secondary[600],
+            fontSize: 8,
+            color: colors.secondary,
+            textAlign: 'center'
+          }}>{item.judul}</Text>
+        </View>
+      </TouchableWithoutFeedback>
+    )
+  }
+
+
+  return (
+
+    <SafeAreaView style={{
+      flex: 1,
+      backgroundColor: colors.white,
+      position: 'relative'
+    }}>
+
+      <View style={{
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        zIndex: 0
+      }}>
+        <Image source={require('../../assets/top2.png')} style={{
+          width: 100,
+          height: 140,
+        }} />
+      </View>
+
+
+
+
+      <View style={{
+        paddingHorizontal: 10,
+      }}>
+
+        <View style={{
+          padding: 10,
+        }}>
+          <View style={{
+            flexDirection: 'row',
+            alignItems: 'center'
+          }}>
+            <Image source={require('../../assets/logo.png')} style={{
+              width: 70,
+              height: 70,
+            }} />
+            <View style={{
+              paddingLeft: 10,
+            }}>
+              <Text style={{
+                fontFamily: fonts.secondary[600],
+                fontSize: 15,
+                color: colors.black
+              }}>Selamat datang,  </Text>
+              <Text style={{
+                fontFamily: fonts.secondary[800],
+                fontSize: 15,
+                color: colors.black
+              }}>{user.nama_lengkap}</Text>
+            </View>
+          </View>
+
+          <Text style={{
+            fontFamily: fonts.secondary[800],
+            color: colors.primary,
+            fontSize: 20,
+          }}>DANAWARENG</Text>
+          <Text style={{
+            fontFamily: fonts.secondary[600],
+            fontSize: 15,
+            color: colors.secondary
+          }}>DATABASE PELAYANAN WARGA SRENGSENG</Text>
+        </View>
+      </View>
+
+      <View style={{
+        flex: 1,
+        padding: 10,
+      }}>
+        {data.map(i => {
+          return (
+
+            <TouchableNativeFeedback onPress={() => navigation.navigate('Kategori', i)}>
+              <View style={{
+                flex: 1,
+                position: 'relative',
+                borderRadius: 10,
+                borderColor: colors.secondary,
+                margin: 10,
+                padding: 10,
+                borderWidth: 1,
+                flexDirection: 'row',
+                alignItems: 'center',
+                overflow: 'hidden'
+              }}>
+
+                <View style={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  zIndex: 0
+                }}>
+                  <Image source={require('../../assets/top.png')} style={{
+                    width: 80,
+                    height: 65,
+                  }} />
+                </View>
+                <Image source={{
+                  uri: i.image
+                }} style={{
+                  width: windowWidth / 5,
+                  resizeMode: 'contain',
+                  height: '100%',
+                }} />
+                <Text style={{
+                  flex: 1,
+                  marginLeft: 10,
+                  fontFamily: fonts.secondary[600],
+                  fontSize: 20,
+                }}>{i.judul}</Text>
+              </View>
+            </TouchableNativeFeedback>
+          )
+        })}
+      </View>
+      {/* navigation bottom */}
+      <View style={{
+        height: 60,
+        alignItems: 'center',
+        flexDirection: 'row',
+        borderTopLeftRadius: 40,
+        borderTopRightRadius: 40,
+        backgroundColor: colors.secondary,
+        justifyContent: 'space-around'
+      }}>
+        <TouchableOpacity style={{
+          padding: 10,
+        }}>
+          <Icon type='ionicon' name='home' color={colors.white} size={20} />
+
+
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate('Feature')} style={{
+          padding: 10,
+        }}>
+          <Icon type='ionicon' name='grid' color={colors.white} size={20} />
+
+
+        </TouchableOpacity>
+
+
+
+        <TouchableOpacity onPress={() => navigation.navigate('Account')} style={{
+          padding: 10,
+        }}>
+          <Icon type='ionicon' name='person' color={colors.white} size={20} />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView >
+  )
+}
+
+const styles = StyleSheet.create({
+  tulisan: {
+    fontSize: 14,
+    marginBottom: 10,
+    fontFamily: fonts.secondary[600],
+    color: colors.black,
+    textAlign: 'justify'
+  },
+  tulisanJudul: {
+    fontSize: 16,
+    marginBottom: 10,
+    fontFamily: fonts.secondary[800],
+    color: colors.black,
+    textAlign: 'justify'
+  }
+})
---
+++ src/pages/Splash/index.js
@@ -0,0 +1,101 @@
+import React, { useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  Image,
+  Animated,
+  ImageBackground,
+  SafeAreaView,
+} from 'react-native';
+import { MyButton } from '../../components';
+import { colors, fonts, windowHeight, windowWidth } from '../../utils';
+import { MYAPP, getData } from '../../utils/localStorage';
+
+export default function Splash({ navigation }) {
+
+
+
+  const __getStarted = () => {
+    getData('user').then(res => {
+      if (!res) {
+        navigation.replace('Register')
+      } else {
+        // navigation.replace('GetStarted')
+        navigation.replace('Home')
+      }
+    })
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      getData('user').then(res => {
+        if (res) {
+          navigation.replace('Home')
+        }
+      })
+    }, 1200)
+  }, [])
+
+  return (
+    <SafeAreaView style={{
+      flex: 1,
+      padding: 0,
+      backgroundColor: colors.white,
+      justifyContent: 'center',
+      position: 'relative'
+
+    }}>
+
+
+      <View style={{
+        position: 'absolute',
+        top: 0,
+        zIndex: 99
+      }}>
+        <Image source={require('../../assets/top.png')} style={{
+          width: 150,
+          height: 140,
+        }} />
+      </View>
+
+
+
+
+
+      <View style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <Image source={require('../../assets/logo.png')} style={{
+          width: windowWidth,
+          height: windowWidth,
+          resizeMode: 'contain'
+        }} />
+
+        <Text style={{
+          fontFamily: fonts.secondary[800],
+          fontSize: 30,
+        }}>{MYAPP}</Text>
+        <Text style={{
+          fontFamily: fonts.secondary[600],
+          fontSize: 16,
+          textAlign: 'center'
+        }}>OPTIMALISASI PENGELOLAAN KEARSIPAN MELALUI DATABASE PELAYANAN WARGA SRENGSENG</Text>
+
+      </View>
+
+      <View style={{
+        marginBottom: 20,
+        padding: 10,
+      }}>
+        <MyButton title="Get Started" onPress={__getStarted} />
+      </View>
+
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
